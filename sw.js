8245de8ca963b00428ca838d129ba849044df875
LesterGallagher <LesterGallagher@gmail.com>
2018-06-27 16:25:08 +0200

    pwa

--- sw.js
+++ sw.js
@@ -26,7 +26,47 @@ self.addEventListener('install', function (event) {
         caches.open(STATIC_CACHE).then(function (cache) {
             return cache.addAll(
                 [
-                    
+                    "./",
+                    "./index.html",
+                    "./css/lib/onsen/css/onsenui.min.css",
+                    "./css/lib/onsen/css/onsen-css-components.min.css",
+                    "./css/style.min.css",
+                    "./cordova.js",
+                    "./js/app.min.js",
+                    "https://cdn.polyfill.io/v2/polyfill.min.js",
+                    "./manifest.json",
+                    "./css/lib/onsen/css/ionicons/css/ionicons.min.css",
+                    "./css/lib/onsen/css/material-design-iconic-font/css/material-design-iconic-font.min.css",
+                    "./css/lib/onsen/css/font_awesome/css/font-awesome.min.css",
+                    "./cordova_plugins.js",
+                    "./plugins/cordova-plugin-admobpro/www/AdMob.js",
+                    "./plugins/cordova-plugin-device/www/device.js",
+                    "./plugins/cordova-plugin-device/src/browser/DeviceProxy.js",
+                    "./plugins/cordova-plugin-dialogs/www/notification.js",
+                    "./plugins/cordova-plugin-dialogs/www/browser/notification.js",
+                    "./data/gemeente-codes.json",
+                    "./views/splitter.html",
+                    "./images/meta-icons/favicon-32x32.png",
+                    "./images/meta-icons/android-chrome-144x144.png",
+                    "./js/browser.js",
+                    "./images/meta-icons/favicon-16x16.png",
+                    "./views/map.html",
+                    "./views/map.html",
+                    "./images/meta-icons/android-chrome-192x192.png",
+                    "./css/lib/onsen/css/material-design-iconic-font/fonts/Material-Design-Iconic-Font.woff2",
+                    "./views/about.html",
+                    "./images/screenshot1.png",
+                    "./images/screenshot2.png",
+                    "./images/screenshot3.png",
+                    "./images/screenshot4.png",
+                    "./images/screenshot5.png",
+                    "./views/search.html",
+                    "./views/details.html",
+                    "./views/details-general.html",
+                    "./views/details-panden.html",
+                    "./views/details-verblijfplaatsen.html",
+                    "./views/details-misc.html",
+                    "./views/details-verblijfobject-detail.html"
                 ]
             );
         })
